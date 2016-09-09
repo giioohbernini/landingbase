fc28f8134fa896008e8fcee63a8dc118a7c7a8a4
Giovanni Bernini <giovannibernini.ti@gmail.com>
2016-09-09 18:23:28 -0300

    New commit

---
+++ envia.php
@@ -0,0 +1,23 @@
+<?php
+if (isset($_POST)) {
+	$name = $_POST['inputName'];
+	$email = $_POST['inputEmail'];
+	$telefone = $_POST['inputTelefone'];
+	$to = 'giovanni@ideaestudio.com.br'; 
+	$subject = 'Formulário de contato - Grupo FKR';
+	$headers  = 'MIME-Version: 1.0' . "\r\n";
+	$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+	$headers .= 'From: Formulário GrupoFKR <mtakuma@terra.com.br>' . "\r\n";
+	$redirect = "http://hugny.com.br/sites/fkrlanding/";
+
+	$body ="Novo formulário enviado por: <br><br>Nome: $name<br><br>E-Mail: $email<br><br> Telefone: $telefone";
+	
+	if (mail ($to, $subject, $body, $headers)) {
+		// echo "<script>window.history.back();</script>";
+		// echo "<script>alert('Obrigado por entrar em contato!');</script>";
+		header("location:$redirect");	
+	} else {
+		echo "Não enviado";
+	}
+}
+?>
